d88b146122de6137c00d7c7ebce82ac10c0ea9b9
Halil Kürel <kurelhalil34@gmail.com>
2023-10-14 17:17:47 +0300

    StudentRegistration Added

--- HK.VocationalSchoolAutomason.Api/Controllers/StudentRegistrationController.cs
+++ HK.VocationalSchoolAutomason.Api/Controllers/StudentRegistrationController.cs
@@ -1,4 +1,6 @@
 ﻿using HK.VocationalSchoolAutomason.Bussiness.Interfaces;
+using HK.VocationalSchoolAutomason.Dtos.SchoolDtos.DayDtos;
+using HK.VocationalSchoolAutomason.Dtos.SchoolDtos.StudentRegistrationDtos;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,7 +10,27 @@ namespace HK.VocationalSchoolAutomason.Api.Controllers
     [ApiController]
     public class StudentRegistrationController : ControllerBase
     {
+        private readonly IStudentRegistration _service;
 
+        public StudentRegistrationController(IStudentRegistration service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllJoins()
+        {
+            var result = _service.GetAllStudents();
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAsync([FromQuery] StudentRegistrationCreateDto dto)
+        {
+            var response = await _service.Create(dto);
+            return Created(string.Empty, response);
+
+        }
 
     }
 }
